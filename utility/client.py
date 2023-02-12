b912b750e615514f1bd020c094109571dbcacc07
lakshayGMZ <lakshaygulati1898@gmail.com>
2023-02-12 11:01:46 +0530

    quick changes

--- utility/client.py
+++ utility/client.py
@@ -1,13 +1,11 @@
 import requests
 import config
-
+from utility.cookieGen import CookieGenerator
 
 class ClientSession(requests.Session):
-
     def __init__(self):
         super().__init__()
         self.proxies = config.PROXY
         if config.USER_AGENT:
             self.headers['user-agent'] = config.USER_AGENT
-
-
+        CookieGenerator(self).instagramCookie()
